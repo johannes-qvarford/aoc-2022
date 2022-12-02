2407ff043cbc327b60854367230b7d65a4a88b03
Johannes Qvarford <jq.email+github@pm.me>
2022-12-02 22:59:24 +0100

    Day 2 formatting.

--- src/day2.rs
+++ src/day2.rs
@@ -58,7 +58,7 @@ impl Rps {
 enum Choice {
     X,
     Y,
-    Z
+    Z,
 }
 
 impl Choice {
@@ -94,10 +94,13 @@ struct Match {
 impl Match {
     fn star1_total_score(&self) -> i32 {
         let player_rps: Rps = self.player.into();
-        let round_score = 
-            if player_rps.beats() == self.opponent { 6 }
-            else if self.opponent.beats() == player_rps { 0 }
-            else { 3 };
+        let round_score = if player_rps.beats() == self.opponent {
+            6
+        } else if self.opponent.beats() == player_rps {
+            0
+        } else {
+            3
+        };
         player_rps.shape_score() + round_score
     }
 
