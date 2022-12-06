5dcf4b7e46da4e4abb0a49db70d4250f521dad9e
Johannes Qvarford <jq.email+github@pm.me>
2022-12-06 19:29:25 +0100

    Day 6 part 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,5 +1,5 @@
+use crate::problem::{parse, part1, part2, INPUT_STR};
 use color_eyre::eyre::Result;
-use crate::problem::{parse, INPUT_STR, part1, part2};
 
 mod problem;
 
@@ -8,6 +8,6 @@ fn main() -> Result<()> {
     let parsed = parse(INPUT_STR)?;
 
     println!("part1: {:?}", part1(&parsed));
-    println!("part2: {:?}", part2(&parsed));
+    println!("part2: {:?}", part2(&parsed, 14));
     Ok(())
 }
--- day6/src/problem.rs
+++ day6/src/problem.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 use color_eyre::eyre::Result;
 use itertools::Itertools;
 
@@ -10,15 +12,59 @@ pub(crate) fn parse(s: &str) -> Result<Input> {
 }
 
 pub(crate) fn part1(input: &Input) -> Output {
-    let entry = input.chars().tuple_windows::<(char, char, char, char)>()
+    let entry = input
+        .chars()
+        .tuple_windows::<(char, char, char, char)>()
         .enumerate()
         .find(|&(_, (a, b, c, d))| [a, b, c, d].into_iter().all_unique())
         .expect("There should be at least one unique sequence of 4 chars");
     entry.0 + 4
 }
 
-pub(crate) fn part2(input: &Input) -> Output {
-    10
+pub(crate) fn part2(input: &Input, marker_len: usize) -> Output {
+    fn shrink(a: u8) -> usize {
+        (a - b'a') as usize
+    }
+
+    let buffer_size = marker_len;
+    let mut buffer = VecDeque::new();
+    let bytes = input.as_bytes();
+
+    let mut byte_counts = [0usize; 26];
+    let mut duplicates = 0;
+
+    for byte in bytes.iter().take(buffer_size) {
+        let shrunk = shrink(*byte);
+        buffer.push_back(shrunk);
+        byte_counts[shrunk] += 1;
+
+        if byte_counts[shrunk] > 1 {
+            duplicates += 1;
+        }
+    }
+
+    for (i, shrunk) in bytes[buffer_size..].iter().map(|b| shrink(*b)).enumerate() {
+        byte_counts[shrunk] += 1;
+        buffer.push_back(shrunk);
+        if byte_counts[shrunk] != 1 {
+            duplicates += 1;
+        }
+
+        let previous = buffer
+            .pop_front()
+            .expect("We always push at least as much as we pop");
+        byte_counts[previous] -= 1;
+
+        if byte_counts[previous] != 0 {
+            duplicates -= 1;
+
+            if duplicates == 0 {
+                return i + 1 + buffer_size;
+            }
+        }
+    }
+
+    unimplemented!()
 }
 
 pub(crate) const INPUT_STR: &str = include_str!("_input");
@@ -41,14 +87,41 @@ mod test {
     }
 
     #[test]
-    #[ignore]
-    fn part2_example() {
-        assert_eq!(part2(&parse(EXAMPLE_STR).unwrap()), todo!())
+    fn part2_example_small() {
+        assert_eq!(part2(&parse("aaabcaa").unwrap(), 3), 5)
     }
 
     #[test]
-    #[ignore]
+    fn part2_example() {
+        assert_eq!(part2(&parse(EXAMPLE_STR).unwrap(), 14), 19)
+    }
+
+    #[test]
+    fn part2_example2() {
+        assert_eq!(
+            part2(&parse("bvwbjplbgvbhsrlpgdmjqwftvncz").unwrap(), 14),
+            23
+        )
+    }
+
+    #[test]
+    fn part2_example3() {
+        assert_eq!(
+            part2(&parse("nppdvjthqldpwncqszvftbrmjlhg").unwrap(), 14),
+            23
+        )
+    }
+
+    #[test]
+    fn part2_example4() {
+        assert_eq!(
+            part2(&parse("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg").unwrap(), 14),
+            29
+        )
+    }
+
+    #[test]
     fn part2_test() {
-        assert_eq!(part2(&parse(INPUT_STR).unwrap()), Default::default())
+        assert_eq!(part2(&parse(INPUT_STR).unwrap(), 14), 2313)
     }
 }
