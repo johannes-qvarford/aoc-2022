78d1a5520831392c29103dab8983116478414456
Johannes Qvarford <jq.email+github@pm.me>
2022-12-10 11:24:10 +0100

    Day 9 part 2.

--- day9/src/problem.rs
+++ day9/src/problem.rs
@@ -33,14 +33,17 @@ pub(crate) fn part1(input: &Input) -> Output {
     snake_walk(input, 2)
 }
 
-pub(crate) fn part2(_input: &Input) -> Output {
-    42
+pub(crate) fn part2(input: &Input) -> Output {
+    snake_walk(input, 10)
 }
 
 pub(crate) const INPUT_STR: &str = include_str!("_input");
 
 #[cfg(test)]
 const EXAMPLE_STR: &str = include_str!("_example");
+
+#[cfg(test)]
+const EXAMPLE_PART2_STR: &str = include_str!("_example_part2");
 
 #[cfg(test)]
 mod test {
@@ -58,11 +61,11 @@ mod test {
 
     #[test]
     fn part2_test_example() {
-        assert_eq!(part2(&parse(EXAMPLE_STR).unwrap()), 42)
+        assert_eq!(part2(&parse(EXAMPLE_PART2_STR).unwrap()), 36)
     }
 
     #[test]
     fn part2_test_input() {
-        assert_eq!(part2(&parse(INPUT_STR).unwrap()), 42)
+        assert_eq!(part2(&parse(INPUT_STR).unwrap()), 2691)
     }
 }
